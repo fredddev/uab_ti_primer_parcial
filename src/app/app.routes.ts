2b4ab783dd144240f410c5f80f03fd1a2d9a0628
Freddy Vincenty <freddy_vincenty@hotmail.com>
2025-10-13 20:28:12 -0400

    feat: Rutas basicas configuradas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./home/home.component').then(m => m.HomeComponent),
+      },
+      {
+        path: 'servicios',
+        loadComponent: () =>
+          import('./services/services.component').then(m => m.ServicesComponent),
+      },
+      {
+        path: 'contacto',
+        loadComponent: () =>
+          import('./contact/contact.component').then(m => m.ContactComponent),
+      },
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
